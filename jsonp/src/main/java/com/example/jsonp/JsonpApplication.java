69a84093dc8a2f5f348f32383f7124a4bbcce290
arrowSword <sword_arrow_wxj@sina.com>
2018-02-08 17:04:26 +0800

    第一次提交17：04

---
+++ jsonp/src/main/java/com/example/jsonp/JsonpApplication.java
@@ -0,0 +1,12 @@
+package com.example.jsonp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonpApplication.class, args);
+	}
+}
---
+++ jsonp/src/main/java/com/example/jsonp/controller/JavaController.java
@@ -0,0 +1,8 @@
+package com.example.jsonp.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JavaController {
+
+}
